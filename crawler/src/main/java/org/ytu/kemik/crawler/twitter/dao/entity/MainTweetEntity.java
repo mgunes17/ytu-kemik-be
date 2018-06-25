dca5b7ee13c11a7de65d0a3f5e8bc4a7eebf9f1a
Muhammed Emir Özçevik <emirozcevik@gmail.com>
2018-06-25 21:05:52 +0300

    remove unneccesary imports

--- crawler/src/main/java/org/ytu/kemik/crawler/twitter/dao/entity/MainTweetEntity.java
+++ crawler/src/main/java/org/ytu/kemik/crawler/twitter/dao/entity/MainTweetEntity.java
@@ -6,11 +6,8 @@ import java.util.Set;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
-import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
 @Entity
